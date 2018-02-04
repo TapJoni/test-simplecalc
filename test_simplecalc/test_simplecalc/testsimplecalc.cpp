9e7c4415319809a6a719aa1cf971fdc5d57e0513
Tapio Mattila <tapio.mattila@edu.turkuamk.fi>
2018-02-04 19:04:03 +0200

    add project version 1

---
+++ test_simplecalc/test_simplecalc/testsimplecalc.cpp
@@ -0,0 +1,16 @@
+// simplemath.h
+
+// SimpleMath.cpp : Defines the entry point for the console application.
+
+#include "stdafx.h"
+#include <iostream>
+#include "simplemath.h"
+
+
+int main()
+{
+
+	std::cout << cubic(10) << std::endl;
+	return 0;
+}
+
